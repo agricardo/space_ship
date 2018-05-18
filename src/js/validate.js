de7dea1f0a513a403407e57e583d187e47e027b0
agricardo <raranda@techlaunch.io>
2018-05-17 20:50:38 -0400

    first commit

---
+++ src/js/validate.js
@@ -0,0 +1,15 @@
+export const isNode = (o) => {
+    return(
+        typeof Node === "object" ? o instanceof Node :
+        o && typeof o === "object" && typeof o.nodeType === "number" &&  typeof o.nodeName === "string"  
+     );
+}
+
+
+export const isElement = (o) => {
+    return (
+        typeof HTMLElement === "object" ? o instanceof HTMLElement
+        :
+        o && typeof o === "object" && o !== null && o.nodeType === 1 && typeof o.nodeName === "string"
+    );
+}
---
+++ src/resources/index.js
@@ -0,0 +1,12 @@
+export const load = (elements, resolve) => {
+    let resources = {};
+    elements.forEach(elm => {
+        let image = new Image();
+        image.src = elm.src;
+        image.id = elm.id;
+        image.onload = function () {
+            resources[elm.id] = image;
+            resolve(resources);
+        }
+    });
+}
